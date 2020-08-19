c92c44dd6e240f83dc7789341c7c622e9a96ce85
zzz <277162321@qq.com>
2020-08-19 23:35:32 +0800

    init

---
+++ src/test/java/com/bajibaji/tester/TesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajibaji.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
